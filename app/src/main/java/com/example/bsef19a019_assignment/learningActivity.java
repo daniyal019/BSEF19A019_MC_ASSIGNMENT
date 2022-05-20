f82017e499cf3580e50f160a5bfceaf7a0330aa9
daniyal019 <daniyal019@users.noreply.github.com>
2022-05-20 12:28:42 +0500

    4th commit

--- app/src/main/java/com/example/bsef19a019_assignment/learningActivity.java
+++ app/src/main/java/com/example/bsef19a019_assignment/learningActivity.java
@@ -2,13 +2,27 @@ package com.example.bsef19a019_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 public class learningActivity extends AppCompatActivity {
-
+    Button btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
+        btn3=(Button) findViewById(R.id.btn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(learningActivity.this,pictorialDisplay.class);
+                startActivity(intent);
+
+            }
     }
 }
---
+++ app/src/main/java/com/example/bsef19a019_assignment/pictorialDisplay.java
@@ -0,0 +1,22 @@
+package com.example.bsef19a019_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ImageView;
+public class pictorialDisplay extends AppCompatActivity {
+    Button btn3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pictorial_display);
+        ImageView my=findViewById(R.id.btn3)
+
+    }
+}
