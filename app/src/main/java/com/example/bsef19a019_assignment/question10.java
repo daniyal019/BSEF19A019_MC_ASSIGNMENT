51cf0bc3a30bf6d3722ae5a8f0479f8e9bcd28b6
daniyal019 <daniyal019@users.noreply.github.com>
2022-05-21 11:26:31 +0500

    20th commit

---
+++ app/src/main/java/com/example/bsef19a019_assignment/question10.java
@@ -0,0 +1,70 @@
+package com.example.bsef19a019_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class question10 extends AppCompatActivity {
+    Button btn30,btn31,btn32,btn33;
+    TextView txt2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question10);
+        btn30=(Button)findViewById(R.id.btn30);
+        btn31=(Button)findViewById(R.id.btn31);
+        btn32=(Button)findViewById(R.id.btn32);
+        btn33=(Button)findViewById(R.id.btn33);
+        txt2=(TextView) findViewById(R.id.txt2);
+
+        btn30.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                btn30.setBackgroundColor(Color.RED);
+                txt2.setTextColor(Color.RED);
+                txt2.setText("Wrong Choice!!!!");
+
+            }
+        });
+
+        btn31.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                btn31.setBackgroundColor(Color.RED);
+                txt2.setTextColor(Color.RED);
+                txt2.setText("Wrong Choice!!!!");
+            }
+        });
+        btn32.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn32.setBackgroundColor(Color.RED);
+                txt2.setTextColor(Color.RED);
+                txt2.setText("Wrong Choice!!!!");
+
+            }
+        });
+
+        btn33.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                btn33.setBackgroundColor(Color.GREEN);
+                txt2.setTextColor(Color.GREEN);
+                txt2.setText("Correct Answer!!!");
+               // Intent intent= new Intent(question8.this,question9.class);
+               // startActivity(intent);
+            }
+        });
+
+
+    }
+}
