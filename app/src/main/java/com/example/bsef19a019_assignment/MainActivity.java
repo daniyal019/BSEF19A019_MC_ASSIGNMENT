7bbff8aa1dfb5dd03f8f1599bd017de8fe36a13b
daniyal019 <daniyal019@users.noreply.github.com>
2022-05-21 15:13:28 +0500

    22nd commit

--- app/src/main/java/com/example/bsef19a019_assignment/MainActivity.java
+++ app/src/main/java/com/example/bsef19a019_assignment/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bsef19a019_assignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Bundle;
 import android.app.Activity;
@@ -11,7 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-Button btn1,btn2;
+Button btn1,btn2,button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +38,14 @@ btn1.setOnClickListener(new View.OnClickListener() {
 
             }
         });
-
-
+        button3=(Button) findViewById(R.id.button3);
+button3.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Uri webpage = Uri.parse("https://github.com/daniyal019/BSEF19A019_MC_ASSIGNMENT");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+});
     }
 }
