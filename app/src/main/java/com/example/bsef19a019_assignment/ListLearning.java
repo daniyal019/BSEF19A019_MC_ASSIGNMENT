73e001268dcf4e3acfbc165e9fcf0a2950b04f09
daniyal019 <daniyal019@users.noreply.github.com>
2022-06-05 14:48:43 +0500

    50th commit

--- app/src/main/java/com/example/bsef19a019_assignment/ListLearning.java
+++ app/src/main/java/com/example/bsef19a019_assignment/ListLearning.java
@@ -68,227 +68,248 @@ public class ListLearning extends AppCompatActivity {
                 else if(i==1)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
-
                     //intent.putExtra("id",edit.getText().toString());
                     intent.putExtra("apple", R.drawable.ball);
 
                     intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==2)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.cat);
+                    intent.putExtra("name1", "Cat");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==3)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
+                    intent.putExtra("apple", R.drawable.dog);
+                    intent.putExtra("name1", "Dog");
 
-                    //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
-
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==4)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.elephant);
+                    intent.putExtra("name1", "Elephant");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==5)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.fish);
+                    intent.putExtra("name1", "Fish");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==6)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.grapes);
+                    intent.putExtra("name1", "Grapes");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==7)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.hat);
+                    intent.putExtra("name1", "Hat");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==8)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.ink);
+                    intent.putExtra("name1", "Ink");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==9)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.jug);
+                    intent.putExtra("name1", "Jug");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==10)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.kite);
+                    intent.putExtra("name1", "Kite");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==11)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.lemon);
+                    intent.putExtra("name1", "Lemon");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==12)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.melon);
+                    intent.putExtra("name1", "Melon");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==13)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.nest);
+                    intent.putExtra("name1", "Nest");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==14)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.owl);
+                    intent.putExtra("name1", "Owl");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==15)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.parrot);
+                    intent.putExtra("name1", "Parrot");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==16)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.quran);
+                    intent.putExtra("name1", "Quran");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==17)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
-                    //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.rainbow);
+                    intent.putExtra("name1", "Rainbow");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==18)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.star);
+                    intent.putExtra("name1", "Star");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==19)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.triangle);
+                    intent.putExtra("name1", "Triangle");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==20)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.unicorn);
+                    intent.putExtra("name1", "Unicorn");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==21)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.violin);
+                    intent.putExtra("name1", "Violin");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==22)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.watch);
+                    intent.putExtra("name1", "Watch");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==23)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.xylophone);
+                    intent.putExtra("name1", "Xylophone");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==24)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.yacht);
+                    intent.putExtra("name1", "Yacht");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
                 else if(i==25)
                 {
                     Intent intent = new Intent(ListLearning.this, pictorialDisplay.class);
 
                     //intent.putExtra("id",edit.getText().toString());
-                    intent.putExtra("apple", R.drawable.ball);
+                    intent.putExtra("apple", R.drawable.zebra);
+                    intent.putExtra("name1", "Zebra");
 
-                    intent.putExtra("name1", "Ball");
+                    startActivity(intent);
                 }
 
 
